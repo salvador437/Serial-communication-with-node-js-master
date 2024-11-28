c434ec281f102aee9ddce3c12b01b554fb1d7ae9
salvador <salva@gmail,com>
2024-11-28 03:02:19 +0100

    new file:   app.js 	new file:   index.html 	new file:   node_modules/.bin/node-gyp-build 	new file:   node_modules/.bin/node-gyp-build-optional

---
+++ app.js
@@ -0,0 +1,26 @@
+const { SerialPort, ReadlineParser  } = require('serialport')
+// const estado = document.querySelector(".dato")
+// const estado = getElementsByClassName("led")
+const port = new SerialPort({
+  path: 'COM5',
+  baudRate: 9600,
+  
+})
+
+
+port.open(function (err) {
+  if (err) {
+    return console.log('Error opening port: ', err.message)
+    port.close(console.log("puerto cerrado"))
+  }
+  
+})
+
+const parser = new ReadlineParser()
+port.pipe(parser)
+parser.on('data', console.log)
+
+
+
+
+
